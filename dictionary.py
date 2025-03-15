a99c036ac100db9989bdb995e0784c34f7be819c
s298157 <s298157@studenti.polito.it>
2025-03-15 10:06:54 +0100

    Update

--- dictionary.py
+++ dictionary.py
@@ -1,11 +1,28 @@
 class Dictionary:
-    def __init__(self):
-        pass
+
+    def __init__(self, dizionario = {}):
+        self.dizionario = dizionario
+
+    def __repr__(self):
+        return f"{self.dizionario}"
+
+    def __str__(self):
+        return f"{self.dizionario}"
+
+    def readDict(self):
+        with open("dictionary.txt", "r") as file:
+            parole = []
+            for linea in file:
+                parola = linea.split()
+                parole.append(parola)
+            for parola in parole:
+                self.dizionario[parola[0]] = parola[1]
+            return self.dizionario
 
     def addWord(self):
         pass
 
-    def translate(self):
+    def translate(self, parola_aliena):
         pass
 
     def translateWordWildCard(self):
